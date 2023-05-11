9e04e6ea7f1a93d9e26e94ae71c769f4777b41e1
ativacaoulamg <ativacaoulamg@gmail.com>
2023-05-10 23:03:10 -0300

    .

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -43,8 +43,21 @@ class CalcController {
         return (['+','-','*','%','/'].indexOf(value) > -1); //Metodo retorna index valor sinal maior -1,senao tiver sinal . ou = retorna False 
     }
     
+    pushOperation(value){
+        this._operation.push(value);
+        if (this._operation.length > 3) {
+           this.calc(); 
+        }
+    }
+
+    calc(){
+            let last = this._operation.pop();
+            let result = eval(this._operation.join(""));
+            this._operation = [result, last];
+    }
+    
     addOperation(value){
-        console.log('A', value, isNaN(this.getLastOperation()));
+        //console.log('A', value, isNaN(this.getLastOperation()));
         
         if (isNaN(this.getLastOperation())) {
             if(this.isOperator(value)) {
@@ -52,25 +65,24 @@ class CalcController {
                 this.setLastOperation(value);
                 
             }else if(isNaN(value)){
-                //outra coisa
-                console.log(value);
+                console.log('Outra coisa',value);
             } else {
-                this._operation.push(value);
+                this.pushOperation(value);
                 
             } 
             
         } else {
             if (this.isOperator(value)){
-                this._operation.push(value);
+                this.pushOperation(value);
             }
             else{               
                 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                   this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseInt(newValue));
                 
             }
         }
-        console.log(this._operation);
+        //console.log(this._operation);
     }
     
     setError(){
